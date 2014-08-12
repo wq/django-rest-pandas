2e7aac723f5cfd4648e226980a021bcf5feeacf4
S. Andrew Sheppard <andrew@wq.io>
2014-08-12 13:46:57 -0500

    django.setup() for 1.7

--- tests/__init__.py
+++ tests/__init__.py
@@ -4,5 +4,10 @@ os.environ['DJANGO_SETTINGS_MODULE'] = "tests.settings"
 from django.test.utils import setup_test_environment
 setup_test_environment()
 
+# Django 1.7
+import django
+if hasattr(django, 'setup'):
+    django.setup()
+
 from django.core.management import call_command
 call_command('syncdb')
