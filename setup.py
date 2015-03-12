54d381f7c8e1f502080c1ac0fa76320d8993ae2a
S. Andrew Sheppard <andrew@wq.io>
2015-03-11 20:03:19 -0500

    v0.3.0

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ def parse_markdown_readme():
 
 setup(
     name='rest-pandas',
-    version='0.2.1',
+    version='0.3.0',
     author='S. Andrew Sheppard',
     author_email='andrew@wq.io',
     url='https://github.com/wq/django-rest-pandas',
